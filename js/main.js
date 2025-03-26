94afe29f367ca93878ee44394c45f2d96288bfc6
PARKSANGHYUN3 <basira@easymedia.net>
2025-03-26 13:37:13 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+  // smooth scroll
+  const lenis = new Lenis()
+  lenis.on('scroll', (e) => {
+    console.log(e)
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  requestAnimationFrame(raf);
+
+  let tl = gsap.timeline();
+  // 글자 갯수대로 intro 적용
+  $(".intro-word-list").each(function (e) {
+    tl.to(this, {
+      onStart() {
+        $(".intro-word-list").eq(e).addClass("active");
+      }
+    }, e * 0.2);
+  });
+  tl.to({}, {
+    onComplete() {
+      $(".intro-word-list").addClass("upper");
+    }
+  }, "+=.1");
+  tl.to({}, {
+    onComplete() {
+      $(".intro-word-wrap").addClass("empty");
+    }
+  }, "+=.5");
+  tl.to(".intro-slide", {
+    y: "-100%",
+    stagger: 0.05,
+    ease: "quart.inOut",
+    onComplete() {
+      $("body").removeClass("none-scroll");
+      // $(".rotate-img").addClass("active");
+    }
+  });
+  tl.to(".visual-txt", {
+    y: 0,
+    stagger: .05,
+    opacity: 1,
+    ease: "quart.inOut",
+    onComplete() {
+      $(".visual-txt").addClass("active");
+    }
+  })
+  tl.to({}, {
+    onComplete() {
+      $(".intro").addClass("empty");
+    }
+  }, "+=.5");
+
+  gsap.to(".img-box", {
+    scrollTrigger: {
+      trigger: "html, body",
+      start: "top top",
+      end: "bottom bottom",
+      markers: true,
+      scrub: 1,
+    },
+    y: "100rem"
+  })
+
+})
